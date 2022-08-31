47a4a7dc12ec57dff9ace009d03044fbbb396948
Your Name <you@example.com>
2022-09-01 00:25:33 +0530

    [Megha] Add: Data Inserted To a Table

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -16,3 +16,13 @@ ZipCode int,
 PhoneNumber Varchar(10),
 Email varchar(150)
 );
+
+----UC3----
+Insert into Addressbook Values 
+('Megha','Rahul','RR Nagar','Banglore','Karnataka',889900,'9008088767','meghanelogal@gmail.com'),
+('Savita','Patil','SR Nagar','Dharwad','Karnataka',542177,'9008988765','savitapatil@gmail.com'),
+('Shalini','Santosh','Jayanagar','Hubli','Karnataka',665544,'9006788976','shalinisantu@gmail.com'),
+('Prabhu','Shashi','vijay nagar','Belgaum','Karnataka',677899,'9667688543','prabhushashi@gmail.com'),
+('Teju','Chetan','JP nagar','Haveri','Karnataka',665544,'9003422567','tejuchetan@gmail.com');
+
+select * from Addressbook
